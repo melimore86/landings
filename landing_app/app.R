30c7d342a4922a562297af77efc9422bc094fef4
melimore86 <melimore86@ufl.edu>
2018-06-05 15:33:34 -0400

    copying folder

---
+++ landing_app/app.R
@@ -0,0 +1,69 @@
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(shinythemes)
+library(rsconnect)
+
+
+#rsconnect::setAccountInfo(name='oysterprojectck',
+#                          token='F16D04C1548412A5806A896A22342C97',
+#                          secret='QoYg+iiKNQ8wysd6UfA18M4Y3YHortTMsTJT84eo')
+
+
+
+data<- read.csv ("data.csv", header=T)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(theme = shinytheme("yeti"),
+              
+              
+   # Application title
+
+   titlePanel("Landings Comparison"),
+   
+   sidebarLayout(
+      sidebarPanel(
+        selectInput("area1", label= h4("Area"), 
+                    choices=unique(data$area), selected = "Apalach Landings"),
+        
+        selectInput("area2", label= h4("Comparison Area"), 
+                    choices=unique(data$area), selected= "State Landings")
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("plot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  plotInput <- reactive({
+    
+      data <- data %>% 
+        filter(area == input$area1|area == input$area2)
+    
+      
+    ggplot(data=data, aes(x= Year, y= measurement)) +
+      labs(x= "Year", y="")+
+      geom_point( size=2)+
+      geom_line(linetype = "dashed")+
+      scale_x_continuous(limits=c(1986,2017), breaks=c(1986,1990,1994,1998,2002,2006,2010,2014,2018)) +
+      theme(panel.border = element_rect(color = "black", size = 0.5, fill = NA, linetype="solid"),axis.text.x = element_text(angle = 90, hjust = 1, size= 12)) +
+      facet_wrap(~area, ncol=1, scales= "free_y")
+    
+    
+   })
+  
+  output$plot <-renderPlot({
+    plotInput()
+    
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ landing_app/data_script.R
@@ -0,0 +1,78 @@
+# Landings data processing script
+
+library(dplyr)
+
+landings <- read.csv("data/landings.csv", header= T)
+
+#State Data modification
+state_land <- subset(landings, select= c(Year,State_landings))
+state_land$area <- (state_land$area= "State Landings")
+state_land$measurement<- paste(landings$State_landings)
+
+statel<- state_land %>% select(Year, area, measurement)
+
+state_trips <- subset(landings, select= c(Year,State_trips))
+state_trips$area <- (state_trips$area= "State Trips")
+state_trips$measurement<- paste(landings$State_trips)
+
+statet<- state_trips %>% select(Year, area, measurement)
+
+
+state_ptrips <- subset(landings, select= c(Year,State_per_trip))
+state_ptrips$area <- (state_ptrips$area= "State Per Trips")
+state_ptrips$measurement<- paste(landings$State_per_trip)
+
+statept<- state_ptrips %>% select(Year, area, measurement)
+
+
+apalach_land <- subset(landings, select= c(Year,Apalach_landings))
+apalach_land$area <- (apalach_land$area= "Apalach Landings")
+apalach_land$measurement<- paste(landings$Apalach_landings)
+
+apalachl<- apalach_land %>% select(Year, area, measurement)
+
+
+apalach_trips <- subset(landings, select= c(Year,Apalach_trips))
+apalach_trips$area <- (apalach_trips$area= "Apalach Trips")
+apalach_trips$measurement<- paste(landings$Apalach_trips)
+
+apalacht<- apalach_trips %>% select(Year, area, measurement)
+
+
+apalach_ptrips <- subset(landings, select= c(Year,Apalach_per_trip))
+apalach_ptrips$area <- (apalach_ptrips$area= "Apalach Per Trips")
+apalach_ptrips$measurement<- paste(landings$Apalach_per_trip)
+
+apalachpt<- apalach_ptrips %>% select(Year, area, measurement)
+
+suw_land <- subset(landings, select= c(Year,Suw_landings))
+suw_land$area <- (suw_land$area= "Suwannee Landings")
+suw_land$measurement<- paste(landings$Suw_landings)
+
+suwl<- suw_land %>% select(Year, area, measurement)
+
+suw_trips <- subset(landings, select= c(Year,Suw_trips))
+suw_trips$area <- (suw_trips$area= "Suwannee Trips")
+suw_trips$measurement<- paste(landings$Suw_trips)
+
+suwt<- suw_trips %>% select(Year, area, measurement)
+
+
+suw_ptrips <- subset(landings, select= c(Year,Suw_per_trip))
+suw_ptrips$area <- (suw_ptrips$area= "Suwanee per Trip")
+suw_ptrips$measurement<- paste(landings$Suw_per_trip)
+
+suwpt<- suw_ptrips %>% select(Year, area, measurement)
+
+
+#state<- left_join(state_land, state_trips, by= c("Year", "area", "measurement"))
+#apalach<- full_join(apalach_land, apalach_trips, by= c("Year", "area", "measurement"))
+#suw<- full_join(suw_land, suw_trips, by= c("Year", "area", "measurement"))
+#ptrip_sa<- full_join (apalach_ptrips, suw_ptrips,by= c("Year", "area", "measurement"))
+#<- full_join(state, apalach, by= c("Year", "area", "measurement"))
+#data<- full_join(data, state_ptrips,  by= c("Year", "area", "measurement"))
+#data<- data %>% select(Year, area, measurement)
+
+data<- rbind(statel,statet, statept, apalachl,apalacht,apalachpt, suwl, suwt, suwpt)
+
+write.csv(data, file = "data.csv")
